07c35cea7d59957500f7b65299828e9667a1cfd3
Brandon Bob <blbob0208@gmail.com>
2022-11-17 15:50:35 -0600

    saving progress

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <CategoriesScreen />
+    <>
+      <ExpoStatusBar style='dark'/>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='MealsCategories'>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+
   }
 });
